edd41656beb50c4daefa106803b691a507438c24
omarcosvbn <omarcosvbn@gmail.com>
2024-05-17 10:56:28 +0100

    vloecidade

--- Assets/EvolvingCars/TP2/CarFitness.cs
+++ Assets/EvolvingCars/TP2/CarFitness.cs
@@ -47,7 +47,7 @@ namespace GeneticSharp.Runner.UnityApp.Car
 
                 /*YOUR CODE HERE*/
                 /*Note que é executado ao longo da simulação*/
-                fitness = Distance;
+                fitness = Distance + (-EllapsedTime);
                 /*END OF YOUR CODE*/
 
                 c.Fitness = fitness;
